4dd10a2a045e2e79432b0a9e518e78b9a817bc26
Marc Rudolph <mr@ingentis.de>
2015-02-22 13:08:56 +0100

    Prepare backup container

---
+++ backup/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+CONTAINER_NAME=cookiebackup
+IMAGE=registry.eztwo.com:5042/cookiebackup
+IP=192.168.42.70/24
+
+docker rm -f $CONTAINER_NAME
+docker pull $IMAGE
+docker run \
+ --name=$CONTAINER_NAME \
+ --add-host="es:$(contnet ip es)" \
+ -v /usr/serverconf/backup/.dropbox_uploader:/cfg/.dropbox_uploader \
+ -v /usr/data/cookie/pics:/cookie/pics \
+ -d \
+ $IMAGE
+contnet attach $CONTAINER_NAME $IP
